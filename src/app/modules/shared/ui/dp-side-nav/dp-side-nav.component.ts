4459c78b00b2d4346068c38d17cc3c409473bf37
Anik-kumar <anik.kumar.sarker@gmail.com>
2020-04-20 22:23:05 +0600

    added dynamic routes in side-nav.component

--- src/app/modules/shared/ui/dp-side-nav/dp-side-nav.component.ts
+++ src/app/modules/shared/ui/dp-side-nav/dp-side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,220 +8,229 @@ import { Router } from '@angular/router';
 })
 export class DpSideNavComponent implements OnInit {
 
-  leftNavMenu1 = [
-    {
-      name: 'CORE',
-      menu: [
-        {
-          name: 'Dashboard',
-          'active': false,
-          'collapsed': true,
-          style: { 'height': '0px'},
-          subMenu: [
-            {
-              name: 'Default',
-              'collapsed': true,
-              'active': false,
-              'badge': null
-            },
-            {
-              name: 'Multipurpose',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            },
-            {
-              name: 'Affiliate',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: 'INTERFACE',
-      menu: [
-        {
-          name: 'Layout',
-          'active': true,
-          'collapsed': false,
-          style: {},
-          subMenu: [
-            {
-              name: 'Static Nav',
-              'collapsed': true,
-              'active': false,
-              'badge': null
-            },
-            {
-              name: 'Dark Side',
-              'collapsed': false,
-              'active': true,
-              'badge': 'New!'
-            },
-            {
-              name: 'RTL Layout',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            },
-            {
-              name: 'Page Header',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            }
-          ]
-        },
-        {
-          name: 'Components',
-          'active': false,
-          'collapsed': true,
-          style: { 'height': '0px'},
-          subMenu: [
-            {
-              name: 'Alarts',
-              'collapsed': true,
-              'active': false,
-              'badge': null
-            },
-            {
-              name: 'Avaters',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            },
-            {
-              name: 'Badges',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            }
-          ]
-        },
-        {
-          name: 'Utilities',
-          'active': false,
-          'collapsed': true,
-          style: { 'height': '0px'},
-          subMenu: [
-            {
-              name: 'Static Nav',
-              'collapsed': true,
-              'active': false,
-              'badge': null
-            },
-            {
-              name: 'Dark Side',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            },
-            {
-              name: 'Page Header',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            }
-          ]
-        },
-        {
-          name: 'Pages',
-          'active': false,
-          'collapsed': true,
-          style: { 'height': '0px'},
-          subMenu: [
-            {
-              name: 'Static Nav',
-              'collapsed': true,
-              'active': false,
-              'badge': null
-            },
-            {
-              name: 'Dark Side',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            },
-            {
-              name: 'Page Header',
-              'collapsed': true,
-              'active': false,
-              'badge': 'New!'
-            }
-          ]
-        }
-      ]
-    }
-  ];
+  leftNavMenu = [];
+  // leftNavMenu = [
+  //   {
+  //     name: 'CORE',
+  //     menu: [
+  //       {
+  //         name: 'Dashboard',
+  //         'active': false,
+  //         'collapsed': true,
+  //         style: { 'height': '0px'},
+  //         subMenu: [
+  //           {
+  //             name: 'Default',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': null
+  //           },
+  //           {
+  //             name: 'Multipurpose',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           },
+  //           {
+  //             name: 'Affiliate',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     name: 'INTERFACE',
+  //     menu: [
+  //       {
+  //         name: 'Layout',
+  //         'active': true,
+  //         'collapsed': false,
+  //         style: {},
+  //         subMenu: [
+  //           {
+  //             name: 'Static Nav',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': null
+  //           },
+  //           {
+  //             name: 'Dark Side',
+  //             'collapsed': false,
+  //             'active': true,
+  //             'badge': 'New!'
+  //           },
+  //           {
+  //             name: 'RTL Layout',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           },
+  //           {
+  //             name: 'Page Header',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         name: 'Components',
+  //         'active': false,
+  //         'collapsed': true,
+  //         style: { 'height': '0px'},
+  //         subMenu: [
+  //           {
+  //             name: 'Alarts',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': null
+  //           },
+  //           {
+  //             name: 'Avaters',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           },
+  //           {
+  //             name: 'Badges',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         name: 'Utilities',
+  //         'active': false,
+  //         'collapsed': true,
+  //         style: { 'height': '0px'},
+  //         subMenu: [
+  //           {
+  //             name: 'Static Nav',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': null
+  //           },
+  //           {
+  //             name: 'Dark Side',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           },
+  //           {
+  //             name: 'Page Header',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         name: 'Pages',
+  //         'active': false,
+  //         'collapsed': true,
+  //         style: { 'height': '0px'},
+  //         subMenu: [
+  //           {
+  //             name: 'Static Nav',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': null
+  //           },
+  //           {
+  //             name: 'Dark Side',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           },
+  //           {
+  //             name: 'Page Header',
+  //             'collapsed': true,
+  //             'active': false,
+  //             'badge': 'New!'
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // ];
 
 
 
-  leftNavMenu = [
-    {
-      name: 'Dashboard',
-      'active': false,
-      'collapsed': true,
-      style: { 'height': '0px'},
-      'subMenu': [
-        {
-          'name': 'Default',
-          'collapsed': true,
-          'active': false,
-          'badge': null
-        },
-        {
-          'name': 'Multipurpose',
-          'collapsed': true,
-          'active': false,
-          'badge': 'New!'
-        },
-        {
-          'name': 'Affiliate',
-          'collapsed': true,
-          'active': false,
-          'badge': 'New!'
-        }
-      ]
-    },
-    {
-      'name': 'Layout',
-      'active': false,
-      'collapsed': true,
-      style: { 'height': '0px'},
-      'subMenu': [
-        {
-          'name': 'Static Nav',
-          'collapsed': true,
-          'active': false,
-          'badge': null
-        },
-        {
-          'name': 'Dark Side',
-          'collapsed': true,
-          'active': false,
-          'badge': 'New!'
-        },
-        {
-          'name': 'Page Header',
-          'collapsed': true,
-          'active': false,
-          'badge': 'New!'
-        }
-      ]
-    }
-  ]
+  // leftNavMenu = [
+  //   {
+  //     name: 'Dashboard',
+  //     'active': false,
+  //     'collapsed': true,
+  //     style: { 'height': '0px'},
+  //     'subMenu': [
+  //       {
+  //         'name': 'Default',
+  //         'collapsed': true,
+  //         'active': false,
+  //         'badge': null
+  //       },
+  //       {
+  //         'name': 'Multipurpose',
+  //         'collapsed': true,
+  //         'active': false,
+  //         'badge': 'New!'
+  //       },
+  //       {
+  //         'name': 'Affiliate',
+  //         'collapsed': true,
+  //         'active': false,
+  //         'badge': 'New!'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     'name': 'Layout',
+  //     'active': false,
+  //     'collapsed': true,
+  //     style: { 'height': '0px'},
+  //     'subMenu': [
+  //       {
+  //         'name': 'Static Nav',
+  //         'collapsed': true,
+  //         'active': false,
+  //         'badge': null
+  //       },
+  //       {
+  //         'name': 'Dark Side',
+  //         'collapsed': true,
+  //         'active': false,
+  //         'badge': 'New!'
+  //       },
+  //       {
+  //         'name': 'Page Header',
+  //         'collapsed': true,
+  //         'active': false,
+  //         'badge': 'New!'
+  //       }
+  //     ]
+  //   }
+  // ]
+
+  @Input()
+  set sideNavList(navList) {
+    console.log('input sideNavList', navList)
+    //this.activeNavItem = navList;
+    this.leftNavMenu = navList['test_nav'];
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(JSON.stringify(this.leftNavMenu));
   }
 
   onNavItemClick(event) {
     console.log('onNavItemClick: ', event);
-    this.leftNavMenu1.forEach(navItem => {
+    this.leftNavMenu.forEach(navItem => {
       navItem.menu.forEach(item => {
         if (item.name == event) {
           item.collapsed = !item.collapsed;
@@ -236,7 +245,7 @@ export class DpSideNavComponent implements OnInit {
           item.collapsed = true;
           item.subMenu.forEach(subMenu => {
             subMenu.active = false;
-            
+
           });
 
           // item.collapsed = false;
@@ -248,7 +257,7 @@ export class DpSideNavComponent implements OnInit {
 
   onClickSubSubMenu(event) {
     console.log('onClickSubSubMenu: ', event);
-    this.leftNavMenu1.forEach(navItem => {
+    this.leftNavMenu.forEach(navItem => {
       navItem.menu.forEach(subNav => {
         if (subNav.name == event.subMenu) {
           subNav.subMenu.forEach(subSubNav => {
@@ -260,8 +269,8 @@ export class DpSideNavComponent implements OnInit {
             }
           })
         }
-      })      
+      })
     });
-    console.log('>> ', this.leftNavMenu1);
+    console.log('>> ', this.leftNavMenu);
   }
 }
